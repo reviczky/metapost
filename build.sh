3c011323178a221fd45edd37b403f71ee43746f6
Taco Hoekwater <taco@elvenkind.com>
2013-01-02 13:00:00 +0000

    do the make export the same way as luatex

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ then
 elif gmake -v >/dev/null 2>&1
 then
   MAKE=gmake;
+  export MAKE;
   echo "You have a GNU-make installed as gmake; I will use that"
 else
   echo "I can't find a GNU-make; I'll try to use make and hope that works." 
@@ -46,8 +47,6 @@ while [ "$1" != "" ] ; do
   fi ;
   shift ;
 done
-
-export MAKE
 
 #
 STRIP=strip
